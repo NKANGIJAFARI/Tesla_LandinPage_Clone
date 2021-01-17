da83ac04a6f5a1b8ccf6d1a0a7a154f2e2967d89
nkangi jafari <nkangijafari@gmail.com>
2021-01-17 20:27:16 +0400

    Header implemented

---
+++ src/components/Item.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Item = () => {
+    return (
+        <div className="item__container">
+            <div className="item__text">
+                <div className="item__textDesc">
+                    <p>Desc</p>
+                </div>
+            </div>
+            <div className="item__lowerThird">
+                <div className="item__buttons">
+                    
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Item
