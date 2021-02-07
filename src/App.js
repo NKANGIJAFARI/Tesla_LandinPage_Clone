259688e13b78a36d6ebd3243e3999fa59d33ee24
nkangi jafari <nkangijafari@gmail.com>
2021-02-07 04:18:32 +0400

    LongTerm commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,97 @@
 import Header from './components/Header'
+import Accessories from './Images/Desktop-Accessories.jpg'
+import ModelS from './Images/Desktop-ModelS.jpeg'
+import Model3 from './Images/Desktop-Model3.jpeg'
+import ModelX from './Images/Desktop-ModelX.jpeg'
+import ModelY from './Images/Desktop-ModelY.jpeg'
+import SolarPanels from './Images/Desktop-SolarPanels.jpeg'
+import SolarRoof from './Images/Desktop-SolarRoof.jpeg'
+
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      
+
+      <div className="app__itemsContainer">
+        <Item 
+        
+          title='Lowest Cost Solar Panels in America'
+          desc="Money-back guarantee"
+          descLink=""
+          background={SolarPanels}
+          leftBtnText = "Order Now"
+          rightBtnText = "LEARN MORE"
+          twoButtons= 'true'
+        > <Header /></Item>
+
+        <Item 
+          title='Model S'
+          desc="$69,420"
+          descLink=""
+          background={ModelS}
+          leftBtnText = "CUSTOM ORDER"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+          twoButtons= 'true'
+        />
+       <Item 
+          title='Model 3'
+          desc="Money-back guarantee"
+          descLink=""
+          background={Model3}
+          leftBtnText = "CUSTOM ORDER"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+          twoButtons= 'true'
+        />
+        <Item 
+          title='Model X'
+          desc="Money-back guarantee"
+          descLink=""
+          background={ModelX}
+          leftBtnText = "CUSTOM ORDER"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+          twoButtons= 'true'
+        />
+        <Item 
+          title='Model Y'
+          desc="Money-back guarantee"
+          descLink=""
+          background={ModelY}
+          leftBtnText = "CUSTOM ORDER"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+          twoButtons= 'true'
+        />
+        <Item 
+          title=' Solar For New Roofs'
+          desc="Money-back guarantee"
+          descLink=""
+          background={SolarRoof}
+          leftBtnText = "CUSTOM ORDER"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+          twoButtons= 'true'
+        />
+        <Item 
+          title='Accessories'
+          desc="Money-back guarantee"
+          descLink=""
+          background={Accessories}
+          leftBtnText = "SHOP NOW"
+          leftBtnLink = ""
+          rightBtnText = "LEARN MORE"
+          rightBtnLink =""
+        />
+      </div>
     </div>
       
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import Button from './Button'
 
-const Item = () => {
+const Item = (props) => {
+const {
+    title, 
+    desc, 
+    descLink,
+    background,
+    leftBtnText,
+    leftBtnLink,
+    rightBtnText,
+    rightBtnLink,
+    twoButton
+} = props
+
     return (
         <div className="item__container">
+           <img src={background} alt=""/> 
             <div className="item__text">
+                <p>{title}</p>
                 <div className="item__textDesc">
-                    <p>Desc</p>
+                    <p>{desc}</p>
                 </div>
             </div>
             <div className="item__lowerThird">
                 <div className="item__buttons">
+                    <Button imp="prmary" text={leftBtnText} Link={leftBtnLink} />
+                    {twoButton && (
+                            <Button imp="secondary" text={rightBtnText} Link ={rightBtnLink} />
+                        )
+                    }
                     
                 </div>
             </div>
